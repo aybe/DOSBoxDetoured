e2e615b5ed04bfde482bb7f97126169fbc698bdc
aybe <aybe.one@hotmail.com>
2020-09-29 22:14:03 +0200

    Hacking Windows...

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,27 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// this won't work, obviously ...
+	// GetProcAddress(GetModuleHandle(L"ntvdm64.dll"), "NtVdm64CreateProcessInternalW");
+
+	HMODULE module = GetModuleHandle(L"KernelBase.dll");
+
+	if (module == NULL)
+	{
+		std::cout << "Couldn't get module handle, GetLastError is " << GetLastError() << "\n";
+		return 1;
+	}
+
+	FARPROC address = GetProcAddress(module, "CreateProcessInternalW");
+
+	if (address == NULL)
+	{
+		std::cout << "Couldn't get proc address, GetLastError is " << GetLastError() << "\n";
+		return 1;
+	}
+
+	std::cout << "Time to celebrate ?";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
